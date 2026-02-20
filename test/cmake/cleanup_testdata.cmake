12951e5f64fbc5811bccf3d1c3c384936865f142
h-shiono <hato.gnss@gmail.com>
2026-02-20 19:32:33 +0900

    Add regression testing framework and scripts for rnx2rtkp and rtkrcv

---
+++ test/cmake/cleanup_testdata.cmake
@@ -0,0 +1,28 @@
+# cleanup_testdata.cmake
+# Removes files extracted from data/MALIB_OSS_data.tar.gz.
+# Called as a CTest FIXTURES_CLEANUP script.
+#
+# Usage:
+#   cmake -DTESTDATA_DIR=<path/to/data> -P cleanup_testdata.cmake
+
+if(NOT DEFINED TESTDATA_DIR)
+    message(FATAL_ERROR "cleanup_testdata.cmake: TESTDATA_DIR is not defined")
+endif()
+
+set(extracted_files
+    "${TESTDATA_DIR}/2024235L.209.l6"
+    "${TESTDATA_DIR}/MALIB_OSS_data_obsnav_240822-1100.sbf"
+    "${TESTDATA_DIR}/MALIB_OSS_data_obsnav_240822-1100.sbf.tag"
+    "${TESTDATA_DIR}/MALIB_OSS_data_obsnav_240822-1100.obs"
+    "${TESTDATA_DIR}/MALIB_OSS_data_obsnav_240822-1100.nav"
+    "${TESTDATA_DIR}/MALIB_OSS_data_l6e_240822-1100.sbf"
+    "${TESTDATA_DIR}/MALIB_OSS_data_l6e_240822-1100.sbf.tag"
+    "${TESTDATA_DIR}/igs14_20230719_KMD_add.atx"
+)
+
+foreach(f IN LISTS extracted_files)
+    if(EXISTS "${f}")
+        file(REMOVE "${f}")
+        message(STATUS "cleanup_testdata: removed ${f}")
+    endif()
+endforeach()
